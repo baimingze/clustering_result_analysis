929d4b3d69a9c4c655cf34032a6c84696c5f527e
baimingze <baimingze@gmail.com>
2017-07-23 20:58:46 +0800

    changed the table schema to 3 tables

--- project_ratio_analyse.py
+++ project_ratio_analyse.py
@@ -2,6 +2,7 @@ import pymysql.cursors
 import urllib3
 
 
+cluster_props_dict = {}
 
 get_id_peptides_url_pre = "http://www.ebi.ac.uk:80/pride/ws/archive/peptide/count/project/"
 
@@ -10,14 +11,16 @@ http = urllib3.PoolManager()
 def get_id_peptides_no(project_id):
 
     get_id_peptides_url = get_id_peptides_url_pre + project_id
-    response = http.request('GET', get_id_peptides_url, timeout=10.0, retries=10)
-    if response.status == 200:
-        return int(response.data)
-    else:
+    try:
+        response = http.request('GET', get_id_peptides_url, timeout=1000.0, retries=1000)
+        if response.status == 200:
+            return int(response.data)
+        else:
+            print("Failed to get the number of identified peptides for project !", project_id)
+            return -1
+    except:
         print("Failed to get the number of identified peptides for project !", project_id)
-
-
-
+        return -2 
 
 def connect_to_db():
     connection = pymysql.connect(host='localhost',
@@ -39,17 +42,55 @@ def get_projects(table_name, connection):
             connection.commit()
             for result in results:
                 project_id = result.get("project_id")
+                print(project_id)
                 projects.add(project_id)
     finally:
-        pass
-
+        print("Got " + str(len(projects)) + "projects")
+    projects.add('PRD000478')
     return projects
 
 def get_clusters(project_id, table_name, connection):
+    global cluster_props_dict
     #only get the identified spec
-    select_sql = "SELECT cluster_id, cluster_ratio, spectrum_title FROM `" + table_name + "` WHERE " + \
+    select_spec_sql = "SELECT spectrum_title, cluster_fk FROM `" + table_name + "_spec` WHERE " + \
                  "spec_prj_id = '" + project_id + "' AND " +\
                  "is_spec_identified = 1" 
+    N_spec_clustered = 0
+    N_spec_above_clus_ratio = 0
+    N_spec_lower_clus_ratio = 0
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(select_spec_sql)
+            results = cursor.fetchall()
+            connection.commit()
+            for result in results:
+                spectrum_title = result.get("spectrum_title")
+                cluster_fk = result.get("cluster_fk")
+                if cluster_fk not in cluster_props_dict :
+                    select_cluster_sql = "SELECT cluster_ratio, n_spec FROM `" + table_name + "` WHERE " + \
+                       "id = '" + str(cluster_fk) + "'"
+                    cursor.execute(select_spec_sql)
+                    result = cursor.fetchone()
+                    connection.commit()
+                    cluster_ratio = result.get("cluster_ratio")
+                    n_spec = result.get("n_spec")
+                    cluster_props = [cluster_ratio, n_spec] 
+                    cluster_props_dict.update( {cluster_fk:cluster_props})
+                else:
+                    cluster_props = cluster_props_dict.get(cluster_fk)
+                    cluster_ratio = cluster_props[0]
+                    n_spec = cluster_props[1]
+                
+                N_spec_clustered += 1
+                if cluster_ratio >= 0.618:
+                    N_spec_above_clus_ratio += 1
+                else:
+                    N_spec_lower_clus_ratio += 1
+   finally:
+        pass
+
+    """
     cluster_dict = {}
     empty_spectra_list = [] 
     N_spec_clustered = 0
@@ -72,9 +113,8 @@ def get_clusters(project_id, table_name, connection):
                     N_spec_above_clus_ratio += 1
                 else:
                     N_spec_lower_clus_ratio += 1
-    finally:
-        pass
-    return (N_spec_clustered, N_spec_above_clus_ratio, N_spec_lower_clus_ratio, cluster_dict)
+    """
+    return (N_spec_clustered, N_spec_above_clus_ratio, N_spec_lower_clus_ratio)
 
 def print_head():
     print("spectra:")
@@ -84,14 +124,13 @@ def print_head():
     print("")
 
 def main():
-    table_name = "201504_2"
+    table_name = "201504_3"
     connection = connect_to_db()
     projects = get_projects(table_name + "_projects", connection)
-    
     print_head()
     for project_id in projects:
         N_peptides_identified = get_id_peptides_no(project_id)
-        (N_spec_clustered, N_spec_above_clus_ratio, N_spec_lower_clus_ratio, clusters) = get_clusters(project_id, table_name, connection)
+        (N_spec_clustered, N_spec_above_clus_ratio, N_spec_lower_clus_ratio) = get_clusters(project_id, table_name, connection)
         print(project_id, end='\t\t')
         print(str(N_peptides_identified), end='\t\t')
         print(str(N_spec_clustered), end='\t\t')
@@ -105,6 +144,7 @@ def main():
             print("%5.2f"%(100 * N_spec_above_clus_ratio/N_spec_clustered) + "%", end='\t\t')
             print("%5.2f"%(100 * N_spec_lower_clus_ratio/N_spec_clustered) + "%", end='\t\t')
         print('')
+        break 
     return  
     
     print(projects)
